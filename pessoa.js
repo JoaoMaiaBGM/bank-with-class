f848e6a5969aa4bb9f45c705aace300f0078306e
João Maia <102671695+JoaoMaiaBGM@users.noreply.github.com>
2022-06-12 09:53:56 -0300

    feat: bank in JS.

---
+++ pessoa.js
@@ -0,0 +1,13 @@
+class Pessoa extends Cliente {
+
+  constructor (idCliente, tipoCliente, dataCriacao, codBanco, agencia, conta, saldo, nome, cpf, email, telefone, dataDeNascimento) {
+    super(idCliente, tipoCliente, dataCriacao, codBanco, agencia, conta, saldo);
+
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.telefone = telefone;
+    this.dataDeNascimento = dataDeNascimento;
+
+  }
+}
